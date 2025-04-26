43b597570bff8bd0245ab7d6fa2fb8c434d078f9
uykfh@student.kit.edu <benni.steeg@icloud.com>
2025-04-26 18:59:59 +0200

    add circles to svg generator

--- lib/svg/svg.ml
+++ lib/svg/svg.ml
@@ -9,8 +9,12 @@ type svg_path_element =
     | LINETO of float * float
     | LINETO_REL of float * float
 
-(** Type for SVG paths*)
-type svg_path = SVG_PATH of svg_path_element list
+(** Type for SVG elements *)
+type svg_element = 
+    | SVG_PATH of svg_path_element list
+    | CIRCLE of float * float * float
+
+let default_options = "fill=\"none\" stroke=\"black\" stroke-width=\"1\" vector-effect=\"non-scaling-stroke\""
 
 (** [open_svg s] opens a new SVG file with name s and returns a handle for it. *)
 let open_svg s = 
@@ -18,6 +22,12 @@ let open_svg s =
     output_string ch "<?xml version=\"1.0\" ?>\n";
     output_string ch "<svg xmlns=\"http://www.w3.org/2000/svg\">\n";
     SVG ch;;
+
+let add_circle (SVG ch) (CIRCLE (cx, cy, r)) = 
+    output_string ch "<circle ";
+    Printf.fprintf ch "cx=\"%.6f\" cy=\"%.6f\" r=\"%.6f\" " cx cy r;
+    output_string ch default_options;
+    output_string ch " />\n";;
 
 (** [add_path svg p] adds the SVG path [p] to the SVG file [svg] by writing it into the file. *)
 let add_path (SVG ch) (SVG_PATH el) = 
@@ -30,18 +40,32 @@ let add_path (SVG ch) (SVG_PATH el) =
     in let elems = List.rev el in
         output_string ch "<path d=\"";
         List.iter print_path_element elems;
-        output_string ch "\" fill=\"none\" stroke=\"black\" stroke-width=\"1\" vector-effect=\"non-scaling-stroke\"/>\n";;
+        output_string ch "\" ";
+        output_string ch default_options;
+        output_string ch " />\n";;
+
+(** [add svg p] adds svg-element [p] to [svg]. *)
+let add svg p = 
+    match p with 
+    | SVG_PATH _ -> add_path svg p
+    | CIRCLE _ -> add_circle svg p
 
 (** [close svg] closes the SVG file [svg]. Must be called to obtain a valid SVG file. *)
 let close (SVG ch) = 
     output_string ch "</svg>"; 
     close_out ch;;
 
+(** [create_circle x y r] creates a circle with radius [r] and its mid-point at [(x, y)]. *)
+let create_circle cx cy r = CIRCLE (cx, cy, r);;
+
 (** Creates a new SVG path and returns it. *)
 let create_path = SVG_PATH [];;
 
 (** [path_add p e] appends SVG path [p] with SVG path element [e]. *)
-let path_add (SVG_PATH p) e = SVG_PATH (e :: p)
+let path_add p e = 
+    match p with
+    | SVG_PATH l -> SVG_PATH (e :: l)
+    | _ -> raise @@ invalid_arg "you can only add svg_path_elements to a SVG_PATH"
 
 (** [path_moveto p x y] appends SVG path [p] with a moveto path element to coordinates [(x, y)]. *)
 let path_moveto p x y = path_add p (MOVETO (x, y))
--- lib/svg/svg.mli
+++ lib/svg/svg.mli
@@ -1,5 +1,5 @@
 type svg
-type svg_path
+type svg_element
 type svg_path_element = 
     | MOVETO of float * float
     | MOVETO_REL of float * float
@@ -11,14 +11,15 @@ val open_svg : string -> svg
 
 val close : svg -> unit
 
-val add_path : svg -> svg_path -> unit
+val add : svg -> svg_element -> unit
 
-val create_path : svg_path
+val create_path : svg_element
+val create_circle : float -> float -> float -> svg_element
 
-val path_add : svg_path -> svg_path_element -> svg_path
+val path_add : svg_element -> svg_path_element -> svg_element
 
-val path_moveto : svg_path -> float -> float -> svg_path
-val path_moveto_rel : svg_path -> float -> float -> svg_path
-val path_closepath : svg_path -> svg_path
-val path_lineto : svg_path -> float -> float -> svg_path
-val path_lineto_rel : svg_path -> float -> float -> svg_path
+val path_moveto : svg_element -> float -> float -> svg_element
+val path_moveto_rel : svg_element -> float -> float -> svg_element
+val path_closepath : svg_element -> svg_element
+val path_lineto : svg_element -> float -> float -> svg_element
+val path_lineto_rel : svg_element -> float -> float -> svg_element
